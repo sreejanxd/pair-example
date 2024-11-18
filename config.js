790d8d76f01c3dce3c00b773a67e365da82665cb
sreejanxd <oyysreejan@gmail.com>
2024-11-18 14:12:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
-  PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
+  PREFIX: process.env.PREFIX || "Sreejanxd",
+  EMAIL: process.env.EMAIL || "oyysreejan@gmail.com",
+  PASS: process.env.PASS || "sreejan900..",
   MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
-  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
+  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/abdg5l.jpg",
 }
